f0fe1094adfb6212d5cf81c89706865cbb57e4aa
ByeongwooAn <abyungwoo@gmail.com>
2025-04-08 17:20:20 +0900

    feat: MainView.vue 구현 중
    1. 가계부 메인 페이지 UI 구현 중 -> MainView.vue
        - 도넛차트 제외 구현 완료. -> 추후에 도넛차트 구현 예정.
        - DB 데이터는 아직 생성하지 않았으므로, 데이터 삽입은 되어 있지 않음.

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,3 +1,26 @@
+<script setup>
+import "../css/mainview.css";
+</script>
+
 <template>
-    <h1>메인화면 입니다.</h1>
+    <div id="container">
+        <div id="main-view-box">
+            <div id="main-attribute-div1">
+                <div id="recent-deal-first">최근 거래 내역 1</div>
+                <div id="recent-deal">최근 거래 내역 2</div>
+                <div id="recent-deal">최근 거래 내역 3</div>
+                <div id="recent-deal">최근 거래 내역 4</div>
+                <div id="recent-deal">최근 거래 내역 5</div>
+            </div>
+
+            <div id="main-attribute-div2">
+                <div id="recent-deal-first">4월 총 수입</div>
+                <div id="recent-deal">4월 총 지출</div>
+            </div>
+
+            <div id="main-attribute-div3"></div>
+
+            <div id="main-attribute-div4">새 거래 내역 추가 +</div>
+        </div>
+    </div>
 </template>
