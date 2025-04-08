1e18b471eacfc3152396b95d47bdb78195612acf
Lee-gaeun <kmeans203@naver.com>
2025-04-08 17:16:50 +0900

    <타입>: 수입/지출 페이지 ui 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/CashReportView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import CashReportView from '../views/CashFlowView.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
-            path: "/cashflow",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import("../views/CashFlowView.vue"),
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: CashReportView,
+    },
+    {
+      path: '/cashflow',
+      name: 'cashflow',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/CashFlowView.vue'),
+    },
+  ],
 });
 
 export default router;
