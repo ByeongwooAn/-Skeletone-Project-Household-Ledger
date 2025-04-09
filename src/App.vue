f649491c5d3e1cde603457535af30af56270044f
ByeongwooAn <abyungwoo@gmail.com>
2025-04-09 14:29:52 +0900

    merge: 충돌해결 및 병합 완료

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
-import MenuBar from './components/common/MenuBar.vue';
+import { useMediaQuery } from "@vueuse/core";
+import MenuBar from "./components/common/MenuBar.vue";
+import MenuBarMobile from "./components/common/MenuBarMobile.vue";
+
+const isMobile = useMediaQuery("(max-width: 394px)");
 </script>
 
 <template>
-  <div id="container">
-    <MenuBar />
-    <div id="page-rendering-div">
-      <RouterView />
+    <div id="container">
+        <component :is="isMobile ? MenuBarMobile : MenuBar" />
+        <div id="page-rendering-div">
+            <RouterView />
+        </div>
     </div>
-  </div>
 </template>
