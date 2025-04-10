2963edc53240466c162f481f88c3ae2edb1b8a50
Lee-gaeun <kmeans203@naver.com>
2025-04-11 01:24:21 +0900

    fix:다크모드 메뉴 이미지 변경

--- src/views/CashFlowView.vue
+++ src/views/CashFlowView.vue
@@ -5,42 +5,41 @@ import axios from 'axios';
 import '../css/cashflow_PC.css';
 import '../css/cashflow_mobile.css';
 
-const type = ref("income");
+const type = ref('income');
 const nextId = ref(1);
 
 const form = reactive({
-    id: 1,
-    type: "income",
-    date: "",
-    amount: 0,
-    category: "",
-    memo: "",
+  id: 1,
+  type: 'income',
+  date: '',
+  amount: 0,
+  category: '',
+  memo: '',
 });
 
-onMounted(async ()=> {
+onMounted(async () => {
   try {
     const res = await axios.get('http://localhost:3001/cashflows');
     const data = res.data;
     if (data.length > 0) {
-      const maxId = Math.max(...data.map(item => item.id));
+      const maxId = Math.max(...data.map((item) => item.id));
       nextId.value = maxId + 1;
     }
-  }
-  catch(error){
+  } catch (error) {
     console.log('id 최신화 실패: ', error);
   }
-})
+});
 
 const categoryOptions = computed(() => {
-    return categoryData[type.value] || [];
+  return categoryData[type.value] || [];
 });
 const setType = (newType) => {
-    type.value = newType;
-    form.type = newType;
+  type.value = newType;
+  form.type = newType;
 };
 
 const handleSubmit = async () => {
-  form.id = nextId.value;
+  form.id = String(nextId.value);
 
   //  현재 시간 생성 (ISO 문자열 형식)
   const createdAt = new Date().toISOString();
@@ -60,7 +59,6 @@ const handleSubmit = async () => {
     form.category = '';
     form.memo = '';
     nextId.value += 1;
-
   } catch (error) {
     console.error('저장 실패:', error);
   }
@@ -70,9 +68,9 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-    <div class="page-wrapper">
-        <div class="cashflow-container">
-            <!-- <header class="topbar">
+  <div class="page-wrapper">
+    <div class="cashflow-container">
+      <!-- <header class="topbar">
         <button class="menu-button">
           <img src="../asset/menu.png" alt="메뉴" />
         </button>
@@ -80,58 +78,36 @@ const handleSubmit = async () => {
         <img class="logo" src="../asset/logo.png" alt="" />
       </header>
       이부분은 모바일 상단 bar코드입니다! -->
-            <div class="content">
-                <div class="button-group">
-                    <button
-                        :class="{ incomeactive: type === 'income' }"
-                        @click="setType('income')"
-                    >
-                        수입
-                    </button>
-                    <button
-                        :class="{ expenseactive: type === 'expense' }"
-                        @click="setType('expense')"
-                    >
-                        지출
-                    </button>
-                </div>
-                <form @submit.prevent="handleSubmit">
-                    <div class="form-group">
-                        <label>날짜</label>
-                        <input type="date" v-model="form.date" />
-                    </div>
-                    <div class="form-group">
-                        <label>금액</label>
-                        <input
-                            type="number"
-                            v-model="form.amount"
-                            placeholder="ex: 10000"
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label>카테고리</label>
-                        <select v-model="form.category">
-                            <option value="">선택</option>
-                            <option
-                                v-for="item in categoryOptions"
-                                :key="item"
-                                :value="item"
-                            >
-                                {{ item }}
-                            </option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>메모</label>
-                        <input
-                            type="text"
-                            v-model="form.memo"
-                            placeholder="메모를 입력하세요"
-                        />
-                    </div>
-                    <button class="submit-button" type="submit">저장</button>
-                </form>
-            </div>
+      <div class="content">
+        <div class="button-group">
+          <button :class="{ incomeactive: type === 'income' }" @click="setType('income')">수입</button>
+          <button :class="{ expenseactive: type === 'expense' }" @click="setType('expense')">지출</button>
         </div>
+        <form @submit.prevent="handleSubmit">
+          <div class="form-group">
+            <label>날짜</label>
+            <input type="date" v-model="form.date" />
+          </div>
+          <div class="form-group">
+            <label>금액</label>
+            <input type="number" v-model="form.amount" placeholder="ex: 10000" />
+          </div>
+          <div class="form-group">
+            <label>카테고리</label>
+            <select v-model="form.category">
+              <option value="">선택</option>
+              <option v-for="item in categoryOptions" :key="item" :value="item">
+                {{ item }}
+              </option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label>메모</label>
+            <input type="text" v-model="form.memo" placeholder="메모를 입력하세요" />
+          </div>
+          <button class="submit-button" type="submit">저장</button>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
