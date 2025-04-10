199013d1583dd79e0ec07ef20a506d35f1b9265f
yejugittest <kimyeju0123@gmail.com>
2025-04-10 16:50:59 +0900

    임시 커밋: PcButton.vue 수정사항

--- src/components/PcButton.vue
+++ src/components/PcButton.vue
@@ -12,48 +12,6 @@
   </template>
   
   <script setup>
-  
+  import "../css/PcButton.css";
   </script>
-  
-  <style scoped>
-  html, body {
-    margin: 0;
-    padding: 0;
-    background-color: #eee;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
-  
-  .screen {
-    width: 1680px;
-    height: 1027px;
-    background-color: white;
-    display: grid;
-    grid-template-rows: 889px auto 35px;
-    grid-template-columns: 1fr auto 35px;
-  }
-  
-  .circle-button {
-    grid-row: 2 / 3;
-    grid-column: 2 / 3;
-    width: 100px;
-    height: 100px;
-    background-color: #F99810;
-    border: none;
-    border-radius: 50%;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    box-shadow: 0 4px 10px rgba(0,0,0,0.2);
-  }
-  
-  .plus-icon {
-    width: 23px;
-    height: 23px;
-  }
-  </style>
   
