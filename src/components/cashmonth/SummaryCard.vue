8b97e6aaa0ec55f8d7450ee31d1be52ff0e06cbc
yel0732 <yerilee1210@gmail.com>
2025-04-10 10:36:56 +0900

    <refactor> CashStatisticsView 주석 수정 및 css 파일 병합
    1. DetailCard, SummaryCard, TotalCard, statistics css 파일 병합 및 수정
    - statistics 파일로 병합
    - 모바일 버전 media query 추가
    
    2. CashStatisticsView 주석 수정

--- src/components/cashmonth/SummaryCard.vue
+++ src/components/cashmonth/SummaryCard.vue
@@ -13,34 +13,3 @@ defineProps({
   percent: Number,
 });
 </script>
-
-<style scoped>
-.card {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 15px 16px;
-  border-radius: 10px;
-  background-color: white;
-  color: black;
-  max-width: 600px;
-  width: 90%;
-  font-weight: bold;
-  flex-wrap: wrap;
-}
-
-.category {
-  width: 95px;
-  text-align: left;
-  padding-left: 2rem;
-}
-.percent {
-  text-align: left;
-  font-size: 0.9rem;
-}
-.amount {
-  flex: 1;
-  text-align: right;
-  padding-right: 2rem;
-}
-</style>
