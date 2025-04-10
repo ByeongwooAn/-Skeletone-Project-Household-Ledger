11fc5d44cae5f1d640293d424e11d83255f8b22e
kyounmmi <mfpa02@naver.com>
2025-04-10 14:25:33 +0900

    수입/지출 VUE 수정

--- src/components/cashmonth/DoughnutChart.vue
+++ src/components/cashmonth/DoughnutChart.vue
@@ -1,35 +1,27 @@
 <script setup>
-import { Doughnut } from "vue-chartjs";
-import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    ArcElement,
-    CategoryScale,
-    plugins,
-} from "chart.js";
+import { Doughnut } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, plugins } from 'chart.js';
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
 
 const chartData = {
-    labels: ["수입", "지출"],
-    datasets: [
-        {
-            label: "My Dataset",
-            data: [60, 40],
-            backgroundColor: ["#60a5fa", "#f87171"],
-            hoverOffset: 10,
-        },
-    ],
+  labels: ['수입', '지출'],
+  datasets: [
+    {
+      label: 'My Dataset',
+      data: [60, 40],
+      backgroundColor: ['#60a5fa', '#f87171'],
+      hoverOffset: 10,
+    },
+  ],
 };
 
 const chartOptions = {
-    responsive: true,
-    cutout: "20%",
+  responsive: true,
+  cutout: '20%',
 };
 </script>
 
 <template>
-    <Doughnut :data="chartData" :options="chartOptions" />
+  <Doughnut :data="chartData" :options="chartOptions" />
 </template>
