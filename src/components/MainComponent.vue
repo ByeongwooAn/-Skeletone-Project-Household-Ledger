9e9c658c1f633da7979c7dc2b1efc54be14c400a
ByeongwooAn <abyungwoo@gmail.com>
2025-04-10 21:53:40 +0900

    feat: 총 수입/지출 서버 연결 작업 중..

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,6 +2,29 @@
 import "../css/mainview.css";
 import { RouterLink } from "vue-router";
 import DoughnutChart from "./cashmonth/DoughnutChart.vue";
+import ApiService from "@/function/apiService";
+import { ref, onMounted } from "vue";
+
+// ref로 상태 선언
+const incomeTotal = ref(0);
+const expenseTotal = ref(0);
+
+// API 호출 함수
+async function getTypeTotal(type) {
+    try {
+        const res = await ApiService.getTypes(type);
+        return res.data.reduce((sum, item) => sum + item.amount, 0);
+    } catch (err) {
+        console.error(`${type} 불러오기 실패`, err);
+        return 0;
+    }
+}
+
+// 컴포넌트 로딩 시 호출
+onMounted(async () => {
+    incomeTotal.value = await getTypeTotal("income");
+    expenseTotal.value = await getTypeTotal("expense");
+});
 </script>
 
 <template>
@@ -15,8 +38,13 @@ import DoughnutChart from "./cashmonth/DoughnutChart.vue";
         </div>
 
         <div id="main-attribute-div2">
-            <div id="recent-deal-first">4월 총 수입</div>
-            <div id="recent-deal">4월 총 지출</div>
+            <!-- API 값 표시 -->
+            <div id="total-income">
+                총 수입: {{ incomeTotal.toLocaleString() }}원
+            </div>
+            <div id="total-expense">
+                총 지출: {{ expenseTotal.toLocaleString() }}원
+            </div>
         </div>
 
         <div id="main-attribute-div3">
